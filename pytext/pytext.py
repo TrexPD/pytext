46e6b406fc582b6a7b509d8546ff07ec409282cf
TrexPD <leynad157@gmail.com>
2024-01-20 21:33:45 -0300

    [feat] 1. Removido o salvamento automático ao chamar a função de remover linhas vazias. 2. Adicionada a função de remover linha(s) por índice ou lista de índices. 3. Adicionada a função de mostrar todo o texto. 4. Adicionados tratamentos de erros e exceções.

--- pytext/pytext.py
+++ pytext/pytext.py
@@ -1,19 +1,40 @@
 import re
+from rich import print
 
 
-class ParserText:
+class TextParser:
     def __init__(self, path: str):
         self.file_path = path
-        with open(self.file_path, "rt", encoding="utf-8") as file:
+        with open(self.file_path, "rt+", encoding="utf-8") as file:
             self.file_contents = file.read()
 
-    
     def remove_empty_lines(self):
-        modified_content = re.sub(r'^\s*\n', '', self.file_contents, flags=re.MULTILINE)
-        self.save_file(modified_content)
+        try:
+            modified_content: re.sub = re.sub(r'^\s*\n', '', self.file_contents, flags=re.MULTILINE)
+        except Exception as error:
+            return "Error:", error
+        else:
+            self.file_contents = modified_content
 
+    def remove_lines(self, row: int | list[int]):
+        try:
+            contents_list = self.file_contents.splitlines()
+            
+            if isinstance(row, list):
+                inverse_row: list = sorted(row, reverse=True)
+                for list_index in inverse_row:
+                    contents_list.pop(list_index)
+            else:
+                contents_list.pop(row)
+        except Exception as error:
+            return "Error:", error
+        else:
+            self.file_contents = "\n".join(contents_list)
 
     def save_file(self, content: str):
-        with open(self.file_path, "wt", encoding="utf-8") as file:
+        with open(self.file_path, mode="wt", encoding="utf-8") as file:
             file.write(content)
+
+    def show(self):
+        print(self.file_contents)
 
